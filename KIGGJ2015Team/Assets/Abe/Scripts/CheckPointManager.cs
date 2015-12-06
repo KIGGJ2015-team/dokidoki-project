5f6fce6acbd6e8627232cfcd563629af47c0fe56
ralsoon <smallkirby423@yahoo.co.jp>
2015-12-06 10:32:01 +0900

    チェックポイントの追加

---
+++ KIGGJ2015Team/Assets/Abe/Scripts/CheckPointManager.cs
@@ -0,0 +1,101 @@
+﻿// ----- ----- ----- ----- -----
+//
+// CheckPoint
+//
+// 作成日：
+// 作成者：
+//
+// <概要>
+//
+//
+// ----- ----- ----- ----- -----
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[AddComponentMenu("MyScript/CheckPoint")]
+public class CheckPointManager : MonoBehaviour
+{
+	#region 変数
+
+	//[Header("タイトル")]
+    
+	[SerializeField, Tooltip("チェックポイントのデータ")]
+    private List<GameObject> keyItemsData;
+
+    [SerializeField, Tooltip("チェックポイントの個数"), Range(1, 100)]
+    private int checkPointNumber = 1;
+
+
+    #endregion
+
+
+    #region プロパティ
+
+
+
+    #endregion
+
+
+    #region メソッド
+
+	// 初期化処理
+    void Awake()
+    {
+
+    }
+
+    // 更新前処理
+    void Start()
+    {
+        
+    }
+
+    // 更新処理
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag != "CheckPoint")
+        {
+            return;
+        }
+
+        //リストと照合同じものを入れないように
+        foreach(var obj in keyItemsData)
+        {
+            if(other.gameObject == obj)
+            {
+                //無駄な処理をしないように
+                return;
+            }
+        }
+
+        keyItemsData.Add(other.gameObject);
+        GoalOpenCheck();
+    }
+
+    void GoalOpenCheck()
+    {
+        if(keyItemsData.Count >= checkPointNumber)
+        {
+            //ゴールの処理
+            Debug.Log("Goal Open");
+#warning 後で実装
+        }
+    }
+
+    void LostKey()
+    {
+        int count = keyItemsData.Count;
+
+        int lostNumber = Random.Range(0, count-1);
+
+        keyItemsData.RemoveAt(lostNumber);
+    }
+	#endregion
+}
---
+++ KIGGJ2015Team/Assets/Abe/Scripts/PlayerTestMove.cs
@@ -0,0 +1,59 @@
+﻿// ----- ----- ----- ----- -----
+//
+// PlayerTestMove
+//
+// 作成日：
+// 作成者：
+//
+// <概要>
+//
+//
+// ----- ----- ----- ----- -----
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[AddComponentMenu("MyScript/PlayerTestMove")]
+public class PlayerTestMove : MonoBehaviour
+{
+	#region 変数
+
+	//[Header("タイトル")]
+    
+	[SerializeField, Tooltip("移動のスピード")]
+	float speed;
+
+    #endregion
+
+
+    #region プロパティ
+
+
+
+    #endregion
+
+
+    #region メソッド
+
+	// 初期化処理
+    void Awake()
+    {
+
+    }
+
+    // 更新前処理
+    void Start()
+    {
+        
+    }
+
+    // 更新処理
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position.x += Input.GetAxisRaw("Horizontal") * speed;
+        transform.position = position;
+    }
+	#endregion
+}
