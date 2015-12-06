18301314ecc96263e617a18d77a99ab9c568b63e
ryunosukehayashi <m0114346@edu.teu.ac.jp>
2015-12-06 14:25:23 +0900

    プレイヤー移動の追加。 a,dで左右に旋回 旋回と同時に回転はまだできてません

---
+++ KIGGJ2015Team/Assets/HAYASHI/script/Player_Move.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Move : MonoBehaviour {
+
+    [SerializeField, TooltipAttribute("前進スピード")]
+    public float speed;
+    [SerializeField, TooltipAttribute("旋回スピード")]
+    public float RotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(transform.up, RotateSpeed * Input.GetAxisRaw("Horizontal"));
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.Rotate(transform.forward, Vector3.Slerp(Vector3.zero, new Vector3(0, 0, 60), Input.GetAxisRaw("Horizontal")).z);
+        }
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
+	}
+}
