71c7754de819603e2f2c2e3987b2bfbbd2151b14
ralsoon <smallkirby423@yahoo.co.jp>
2015-12-06 10:48:08 +0900

    ゴールオブジェクトの追加

---
+++ KIGGJ2015Team/Assets/Abe/Scripts/Goal.cs
@@ -0,0 +1,67 @@
+﻿// ----- ----- ----- ----- -----
+//
+// Goal
+//
+// 作成日：
+// 作成者：
+//
+// <概要>
+//
+//
+// ----- ----- ----- ----- -----
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[AddComponentMenu("MyScript/Goal")]
+public class Goal : MonoBehaviour
+{
+	#region 変数
+
+	//[Header("タイトル")]
+    
+	//[SerializeField, Tooltip("説明文")]
+	
+    #endregion
+
+
+    #region プロパティ
+
+
+
+    #endregion
+
+
+    #region メソッド
+
+	// 初期化処理
+    void Awake()
+    {
+
+    }
+
+    // 更新前処理
+    void Start()
+    {
+        
+    }
+
+    // 更新処理
+    void Update()
+    {
+        
+    }
+
+    void GoalOpen()
+    {
+        //ゴール解放
+    }
+
+    void CheckKey()
+    {
+
+        //ゴールの処理
+    }
+	#endregion
+}
