0ebd75c32a5114dbf6ee6dc9dcd0bbc2f3450b9a
ryunosukehayashi <m0114346@edu.teu.ac.jp>
2015-12-07 17:43:28 +0900

    プレイヤー上下移動を入れ忘れていたので追加しました。

--- KIGGJ2015Team/Assets/HAYASHI/script/Player_Move.cs
+++ KIGGJ2015Team/Assets/HAYASHI/script/Player_Move.cs
@@ -3,24 +3,43 @@ using System.Collections;
 
 public class Player_Move : MonoBehaviour {
 
+    public enum Yaxis
+    {
+        normal,
+        reverse
+    };
+
     [SerializeField, TooltipAttribute("前進スピード")]
     public float speed;
     [SerializeField, TooltipAttribute("旋回スピード")]
     public float RotateSpeed;
+    [SerializeField, TooltipAttribute("上下反転の有無")]
+    public Yaxis yaxis;
 
+
+    float VRotateSpeed;
 	// Use this for initialization
 	void Start () {
+        switch (yaxis)
+        {
+            case Yaxis.normal:
+                VRotateSpeed = -RotateSpeed;
+                break;
+            case Yaxis.reverse:
+                VRotateSpeed = RotateSpeed;
+                break;
+        }
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(transform.up, RotateSpeed * Input.GetAxisRaw("Horizontal"));
-        if (Input.GetAxisRaw("Horizontal") > 0)
-        {
-            transform.Rotate(transform.forward, Vector3.Slerp(Vector3.zero, new Vector3(0, 0, 60), Input.GetAxisRaw("Horizontal")).z);
-        }
+        transform.Rotate(transform.up, RotateSpeed * Input.GetAxisRaw("Horizontal"),Space.World);
+        transform.Rotate(Vector3.right, VRotateSpeed * Input.GetAxisRaw("Vertical"),Space.Self);
+
+
 
         transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
+        
 	}
 }
