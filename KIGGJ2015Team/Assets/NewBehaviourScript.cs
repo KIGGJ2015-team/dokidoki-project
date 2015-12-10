9c4ba370cdc0039647f46db19e662a315bc84eba
ralsoon <smallkirby423@yahoo.co.jp>
2015-12-10 18:29:53 +0900

    変更

--- KIGGJ2015Team/Assets/NewBehaviourScript.cs
+++ KIGGJ2015Team/Assets/NewBehaviourScript.cs
@@ -5,11 +5,15 @@ public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+	    Debug.Assert(false, "Start");
+        //a
+        //aa
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    Debug.Assert(false, "Update");
+        //a
+        //aa
 	}
 }
