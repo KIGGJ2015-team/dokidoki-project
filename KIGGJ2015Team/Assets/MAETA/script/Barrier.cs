9626a3af151745f85db2828d38a95a0bbb6a02f5
shogoMS <sinya9613@gmail.com>
2015-12-10 17:38:21 +0900

    qwerykl;

---
+++ KIGGJ2015Team/Assets/MAETA/script/Barrier.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barrier : MonoBehaviour {
+    
+    private int life = 2;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnTriggerEnter(Collider coll)
+    {
+      // coll.gameObject.SendMessage("Reflect");
+        if (coll.gameObject.tag == "Barrier"){
+            Debug.Log("ataru");
+            life--;
+
+            if (life == 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ KIGGJ2015Team/Assets/MAETA/script/move.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+    public float Speed ;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0.0f) * Time.deltaTime * Speed;
+    }
+    void Reflect()
+    {
+       //  GetComponent<Rigidbody>().AddForce(Vector3.up * 15.0f, ForceMode.Impulse);
+    }
+}
