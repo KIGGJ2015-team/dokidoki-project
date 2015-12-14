e83a7607520d661919d955eb1da67139958e2d36
ryunosukehayashi <m0114346@edu.teu.ac.jp>
2015-12-14 14:07:27 +0900

    レティクルの表示を追加

--- KIGGJ2015Team/Assets/HAYASHI/script/Camera_Control.cs
+++ KIGGJ2015Team/Assets/HAYASHI/script/Camera_Control.cs
@@ -5,6 +5,8 @@ public class Camera_Control : MonoBehaviour {
 
     [SerializeField,Tooltip("カメラの回転速度"),Range(0,45)]
     public float rotateSpeed;       //カメラの回転速度
+    [SerializeField, Tooltip("カメラ上下の回転制限")]
+    public float YrotateMin, YrotateMax;
     
 	// Use this for initialization
 	void Start () {
@@ -15,7 +17,7 @@ public class Camera_Control : MonoBehaviour {
 	void Update () {
         transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime * Input.GetAxisRaw("Camera_Horizontal"),Space.World);
         transform.Rotate(Vector3.right, rotateSpeed * Time.deltaTime * Input.GetAxisRaw("Camera_Vertical"), Space.Self);
-        if (transform.localRotation.x <= -30 || transform.localRotation.x >= 80) Debug.Log("rotatelimit");
+        if (transform.localEulerAngles.x <= -30 || transform.localEulerAngles.x >= 80) Debug.Log("rotatelimit");
 	
 	}
 }
--- KIGGJ2015Team/Assets/HAYASHI/script/Player_Reticle.cs
+++ KIGGJ2015Team/Assets/HAYASHI/script/Player_Reticle.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class Player_Reticle : MonoBehaviour {
 
+    Ray ray;
+    RaycastHit hit;
+    public float limitrange;
+    GameObject reticule;
+
 	// Use this for initialization
 	void Start () {
+        reticule = GameObject.Find("reticle");
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        ray = new Ray(transform.position, transform.forward);
+        if(Physics.Raycast(ray,out hit, limitrange))
+        {
+            reticule.transform.position = hit.transform.position;
+        }else
+        {
+            reticule.transform.position = ray.GetPoint(limitrange);
+        }
 	
 	}
 }
