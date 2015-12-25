4ce827fc96aba53827e124bcf9bcc64feb099eb9
ralsoon <smallkirby423@yahoo.co.jp>
2015-12-25 13:31:36 +0900

    セレクトシーン追加　十字キーかマウスで選択 まだ遷移はしません 所要時間3時間30分

---
+++ KIGGJ2015Team/Assets/Abe/Scripts/PlayerSelect.cs
@@ -0,0 +1,124 @@
+﻿// ----- ----- ----- ----- -----
+//
+// PlayerSelect
+//
+// 作成日：
+// 作成者：
+//
+// <概要>
+// セレクトシーンで使うスクリプト
+// プレイヤーを選択します
+//
+// ----- ----- ----- ----- -----
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[AddComponentMenu("MyScript/PlayerSelect")]
+public class PlayerSelect : MonoBehaviour
+{
+	#region 変数
+
+    [SerializeField,
+     Range(0, 3),
+     Tooltip("プレイヤーが現在選択している機体の番号" + "\n" +
+             "初めに選択されている機体を変えられます")]
+    private int state;
+
+    [SerializeField, Tooltip("切り替わるスピード(秒)")]
+    private float changeSpeed;
+
+    Hashtable hash;
+
+    #endregion
+
+
+    #region プロパティ
+    #endregion
+
+
+    #region メソッド
+
+	// 初期化処理
+    void Awake()
+    {
+        hash = new Hashtable();
+        hash.Add("easetype", iTween.EaseType.easeOutQuart);
+        hash.Add("time", changeSpeed);
+    }
+
+    // 更新前処理
+    void Start()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            if(i == state)
+            {
+                transform.GetChild(i).position = Vector3.zero;
+                continue;
+            }
+
+            transform.GetChild(i).position = new Vector3(15, 0, 0);
+        }
+    }
+
+    // 更新処理
+    void Update()
+    {        
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ChangePlayer(false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ChangePlayer(true);
+        }
+    }
+
+    public void ChangePlayer(bool isRightSide)
+    {
+        if(isRightSide)
+        {
+            Change( 1);
+        }
+        else
+        {
+            Change(-1);
+        }
+    }
+
+    private void Change(int direction)
+    {
+        int nowState  = state;
+        int nextState = state + 1 * direction;
+        if(nextState < 0)
+        {
+            nextState = 3;
+        }
+        else
+        if(nextState > 3)
+        {
+            nextState = 0;
+        }
+        
+
+        Vector3  nowPosition = new Vector3(0, 0, 0);
+        Vector3 nextPosition = new Vector3(15 * direction * -1, 0, 0);
+
+        transform.GetChild( nowState).position =  nowPosition;
+        transform.GetChild(nextState).position = nextPosition;
+
+        hash["path"] = new Vector3[] { nowPosition, nowPosition + new Vector3(15 * direction, 0, 0)};        
+
+        iTween.MoveTo(transform.GetChild(nowState).gameObject, hash);
+
+        hash["path"] = new Vector3[] { nextPosition, nextPosition + new Vector3(15 * direction, 0, 0) };
+
+        iTween.MoveTo(transform.GetChild(nextState).gameObject, hash);
+
+        state = nextState;
+    }
+	#endregion
+}
