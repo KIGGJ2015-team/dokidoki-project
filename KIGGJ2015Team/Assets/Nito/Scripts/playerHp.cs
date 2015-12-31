cee76279faa05125f51c0c4656da4f3aeaf45525
unknown <curculin@icloud.com>
2015-12-31 21:58:36 +0900

    HP/連射ゲージ(未：UI差し替え)

---
+++ KIGGJ2015Team/Assets/Nito/Scripts/playerHp.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.VR;
+
+public class playerHp : MonoBehaviour
+{
+
+
+    public Image gage;
+    public float pHp;
+    public float speed = 0.01f;
+
+    // Use this for initialization
+    void Start()
+    {
+        pHp = gage.fillAmount = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        pHp = pHp + speed;
+        if (pHp >= 1)
+        {
+           pHp = 0.01f;
+        }
+
+        gage.fillAmount = pHp;
+    }
+
+    public void AddScore(float Addhp)
+    {
+        pHp = pHp + Addhp;
+    }
+}
