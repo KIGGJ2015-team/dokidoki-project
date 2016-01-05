2f2a37a72b2f11802039cbba0e89c005d4770aa6
ralsoon <smallkirby423@yahoo.co.jp>
2016-01-05 14:51:34 +0900

    コミット

---
+++ KIGGJ2015Team/Assets/MAETA/script/title.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class title : MonoBehaviour {
+
+    [SerializeField, Tooltip("シーンを管理するオブジェクト")]
+    GameObject sceneManager;
+
+    SceneManager manager;
+
+	// Use this for initialization
+	void Start () {
+	    manager = sceneManager.GetComponent<SceneManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void stage_select()
+    {
+        //機体選択へ
+        manager.SceneChange(SceneName.SelectScene);
+
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
