d272bba9df24956b4d5b96b99ece3411f24ac2ee
ryunosukehayashi <m0114346@edu.teu.ac.jp>
2016-01-10 19:02:31 +0900

    こちらも未完成ですがいったんコミット

---
+++ KIGGJ2015Team/Assets/HAYASHI/script/test/testparticle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testparticle : MonoBehaviour {
+
+    float time = 0;
+    float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
+	
+	}
+}
