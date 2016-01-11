69919c888f9577ebc6976eebc089363f4a56deab
shogoMS <sinya9613@gmail.com>
2016-01-11 22:08:58 +0900

    破壊エフェクト（難あり）BGM、SE付き リザルト画面（スクリプト内の変更要）

--- KIGGJ2015Team/Assets/MAETA/script/result.cs
+++ KIGGJ2015Team/Assets/MAETA/script/result.cs
@@ -3,26 +3,69 @@ using System.Collections;
 
 public class result : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject one;
+    [SerializeField]
+    private GameObject two;
+    [SerializeField]
+    private GameObject three;
+    [SerializeField]
+    private GameObject four;
+    Vector3 ONE;
+    Vector3 TWO;
+    Vector3 THREE;
+    Vector3 FOUR;
+    [SerializeField]
+    public bool goal = false;
+    [SerializeField]
+    bool iti = false;
+    [SerializeField]
+    bool ni = false;
+    [SerializeField]
+    bool san = false;
+    [SerializeField]
+    bool yon = false;
 
-    int score; // 今回のプレイスコアがはいっているものとして
-    int hiscore = PlayerPrefs.GetInt("HISCORE");
+    
     // Use this for initialization
     void Start () {
-	
-	}
+        ONE = one.transform.position;
+        one.transform.position = new Vector3(-10000, -10000, 0);
+
+        TWO = two.transform.position;
+        two.transform.position = new Vector3(-10000, -10000, 0);
+
+        THREE = three.transform.position;
+        three.transform.position = new Vector3(-10000, -10000, 0);
+
+        FOUR = four.transform.position;
+        four.transform.position = new Vector3(-10000, -10000, 0);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        
-        if (score > hiscore)
+
+        if (goal)
         {
-            //scoreをハイスコアとして表示
-            /// 今回のスコアがハイスコアより大きいので保存
-            PlayerPrefs.SetInt("HISCORE", score);
-        }
-        else
-        {
-            // hiscoreをハイスコアとして表示
+            if (iti)
+            {
+              one.transform.position = ONE;
+            }
+
+            if (ni)
+            {
+                two.transform.position = TWO;
+            }
+
+            if (san)
+            {
+                three.transform.position = THREE;
+            }
+
+            if (yon)
+            {
+                four.transform.position = FOUR;
+            }
         }
     }
 }
